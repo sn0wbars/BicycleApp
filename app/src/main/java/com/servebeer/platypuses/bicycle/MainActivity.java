0970c334ed5754a3a173a72643adf81b0f3617f9
sn0wbars <alex-borz@yandex.ru>
2018-12-13 23:16:09 +0300

    Check and request pernissions

--- app/src/main/java/com/servebeer/platypuses/bicycle/MainActivity.java
+++ app/src/main/java/com/servebeer/platypuses/bicycle/MainActivity.java
@@ -1,14 +1,25 @@
 package com.servebeer.platypuses.bicycle;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.e(TAG, "No permission");
+            this.requestPermissions(new String[]{Manifest.permission.INTERNET,
+                    Manifest.permission.ACCESS_NETWORK_STATE}, 0);
+        }
         Intent decideIntent;
         Object token = null;
         if (token != null)
